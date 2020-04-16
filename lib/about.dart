e03c03fa5ee43c40704c10ab7dd8e44ea9675f4b
Isaac Morrow <isaacsenseimorrow@gmail.com>
2020-04-15 23:28:17 -0700

    Added content to About Page

--- lib/about.dart
+++ lib/about.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'drawer.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
 class About extends StatelessWidget {
   @override
@@ -14,14 +15,69 @@ class About extends StatelessWidget {
             fontWeight: FontWeight.bold,
           ),
         ),
-        // centerTitle: true,
+        centerTitle: true,
       ),
       body: Center(
-        child: Column(
-          children: <Widget>[],
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Neumorphic(
+            boxShape: NeumorphicBoxShape.roundRect(
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+            style: NeumorphicStyle(
+              shape: NeumorphicShape.concave,
+              depth: -50.0,
+              lightSource: LightSource.topLeft,
+              color: Colors.grey[100],
+              intensity: 3.0,
+            ),
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 15.0, vertical: 20.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  CircleAvatar(
+                    backgroundImage: AssetImage('assets/isaacmorrow.jpg'),
+                    radius: 100.0,
+                  ),
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                  Text(
+                    "Isaac Morrow",
+                    style: TextStyle(
+                        fontSize: 24.0,
+                        letterSpacing: 3.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    "Software Developer",
+                    style: TextStyle(fontSize: 18.0),
+                  ),
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      '\"Hamlet\" is an app created by Isaac Morrow for his English 12 final project on Shakespear\'s play, Hamlet. All code is written by Isaac Morrow in 2020.',
+                      style: TextStyle(fontSize: 16.0),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  SizedBox(height: 10.0),
+                  Text(
+                    "Also, Mr. Murphy is cool.",
+                    style: TextStyle(fontSize: 10.0),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
-      drawer: customDrawer(),
+      drawer: CustomDrawer(),
     );
   }
 }
