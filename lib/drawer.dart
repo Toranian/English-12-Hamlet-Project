e934b63f82bb9b1e94057e2ba63ba8a4f3568ab6
Isaac Morrow <isaacsenseimorrow@gmail.com>
2020-04-15 23:28:53 -0700

    Added page routing

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -1,114 +1,90 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+import 'package:route_transitions/route_transitions.dart';
 
 import 'setting.dart';
 import 'themes.dart';
 import 'about.dart';
+import 'characters.dart';
+import 'main.dart';
 
-Drawer customDrawer() {
-  return Drawer(
-    child: Column(
-      children: <Widget>[
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            UserAccountsDrawerHeader(
-              accountName: Text(
-                "Hamlet",
+class CustomDrawer extends StatelessWidget {
+  final v_p = 10.0;
+  final h_p = 15.0;
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              UserAccountsDrawerHeader(
+                currentAccountPicture: Image.asset('assets/crown.png'),
+                accountName: Text(
+                  "Hamlet",
+                  style: TextStyle(color: Colors.white, fontSize: 20.0),
+                  textAlign: TextAlign.center,
+                ),
+                accountEmail: Text(
+                  "An English 12 Project",
+                  style: TextStyle(color: Colors.white, fontSize: 16.0),
+                ),
+                decoration: BoxDecoration(color: Colors.purple),
               ),
-              accountEmail: Text("Skeet skeet"),
-              decoration: BoxDecoration(color: Colors.pink[400]),
-            ),
 
-            SizedBox(height: 50.0),
+              SizedBox(height: 20.0),
 
-            // Setting
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: NeumorphicButton(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    "Setting",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-                style: NeumorphicStyle(
-                  // shape: NeumorphicShape.concave,
-                  depth: 10,
-                  lightSource: LightSource.topLeft,
-                  color: Colors.white, intensity: 1.5,
-                ),
-              ),
-            ),
+              // Characters
+              drawerButton(v_p, h_p, 'Characters', context, Characters()),
 
-            // Themes
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: NeumorphicButton(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    "Themes",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-                style: NeumorphicStyle(
-                  // shape: NeumorphicShape.concave,
-                  depth: 10,
-                  lightSource: LightSource.topLeft,
-                  color: Colors.white, intensity: 1.5,
-                ),
-              ),
-            ),
+              // Setting
+              drawerButton(v_p, h_p, 'Setting', context, Setting()),
+              // Themes
+              drawerButton(v_p, h_p, 'Themes', context, Themes()),
 
-            // Characters
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: NeumorphicButton(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    "Characters",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-                style: NeumorphicStyle(
-                  // shape: NeumorphicShape.concave,
-                  depth: 10,
-                  lightSource: LightSource.topLeft,
-                  color: Colors.white, intensity: 1.5,
-                ),
-              ),
-            ),
+              // About
+              drawerButton(v_p, h_p, 'About', context, About()),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
 
-            // About
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: NeumorphicButton(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    "About",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-                style: NeumorphicStyle(
-                  // shape: NeumorphicShape.concave,
-                  depth: 10,
-                  lightSource: LightSource.topLeft,
-                  color: Colors.white, intensity: 1.5,
-                ),
-              ),
-            ),
-          ],
+Padding drawerButton(
+  double v_p,
+  double h_p,
+  String name,
+  BuildContext context,
+  var route,
+) {
+  return Padding(
+    padding: EdgeInsets.symmetric(horizontal: h_p, vertical: v_p),
+    child: NeumorphicButton(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(
+          name,
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0),
+          textAlign: TextAlign.center,
         ),
-      ],
+      ),
+      style: NeumorphicStyle(
+        // shape: NeumorphicShape.concave,
+        depth: 10,
+        lightSource: LightSource.topLeft,
+        color: Colors.white, intensity: 1.5,
+      ),
+      onClick: () {
+        Navigator.of(context).push(
+          PageRouteTransition(
+              animationType: AnimationType.slide_right,
+              builder: (context) => route),
+        );
+      },
     ),
   );
 }
