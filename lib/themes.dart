89a6ed9fbd3f21146f3b889ffff7390bca8032bd
Isaac Morrow <isaacsenseimorrow@gmail.com>
2020-04-20 21:45:13 -0700

    Finalized themes

--- lib/themes.dart
+++ lib/themes.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'drawer.dart';
+import 'style.dart';
 
 class Themes extends StatelessWidget {
   @override
@@ -17,10 +17,24 @@ class Themes extends StatelessWidget {
         centerTitle: true,
         // centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          children: <Widget>[],
-        ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        mainAxisSpacing: 4.0,
+        crossAxisSpacing: 4.0,
+        children: <Widget>[
+          GridItem(
+            context,
+            'skull.png',
+            'Death',
+            'The theme of death is consistent throughout the play. For most of the play, Hamlet questions the mystery of death and whether or not he should end his life. He believes that life is so cruel already that death seems more appealing. Also, nearly everyone dies. Shakespear enjoys making tragic plays, so this shouldn\'t come as a surprise.',
+          ),
+          GridItem(
+            context,
+            'insanity.jpg',
+            'Insanity',
+            'Near the beginning of the play, Hamlet decides that he shall pretend to be insane so he can get away with crazier acts. However, near the end of the play, it becomes clear that he may not be pretending anymore and may actually be insane. It is unkown if he is truly mad, and is one of the main themes of the play.',
+          ),
+        ],
       ),
       drawer: CustomDrawer(),
     );
